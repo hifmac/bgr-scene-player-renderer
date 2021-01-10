8aba350e68969f1c3e5bc7d17ade54ed12df5c37
hifmac <73805644+hifmac@users.noreply.github.com>
2021-01-10 18:03:43 +0900

    implement test page for web access

--- docs/js/page.js
+++ docs/js/page.js
@@ -1,4 +1,3 @@
-
 /**
  * @file page.js
  * @description Main page script loaded from page.html
@@ -10,78 +9,159 @@
 
 import {
     onLoad,
-    getURLParameter
+    getURLParameter,
+    BGRSP
 } from './blanc/lisette.js';
 
-import Background from './components/background.js';
-import Scenario from './components/scenario.js';
-import Check from './components/check.js';
-import Search from './components/search.js';
-import TexturePreview from './components/texture-preview.js';
-import CharacterEditor from './components/character-editor.js';
+/**
+ * 
+ * @param {typeof import('./components/background.js').default} Background 
+ * @param {typeof import('./components/scenario.js').default} Scenario 
+ * @param {typeof import('./components/check.js').default} Check 
+ * @param {typeof import('./components/search.js').default} Search 
+ * @param {typeof import('./components/texture-preview.js').default} TexturePreview 
+ * @param {typeof import('./components/character-editor.js').default} CharacterEditor 
+ */
+function drawPage(
+    Background,
+    Scenario,
+    Check,
+    Search,
+    TexturePreview,
+    CharacterEditor) {
 
-onLoad(function (config) {
-    const background = new Background('#background', config);
-    background.show();
-
-    let currentView = null;
-    const scenario = new Scenario('#app', getURLParameter().v, config);
-    const check = new Check('#app', config);
-    const search = new Search('#app', config);
-    const texturePreview = new TexturePreview('#app', config);
-    const characterEditor = new CharacterEditor('#app', config);
-
-    scenario.addViewChangeListener(function(view) {
-        if (currentView) {
-            currentView.destroy();
-            currentView = null;
+    onLoad((config) => {
+        const background = new Background('#background', config);
+        background.show();
+    
+        let currentView = null;
+        const scenario = new Scenario('#app', getURLParameter().v, config);
+        const check = new Check('#app', config);
+        const search = new Search('#app', config);
+        const texturePreview = new TexturePreview('#app', config);
+        const characterEditor = new CharacterEditor('#app', config);
+    
+        scenario.addViewChangeListener(function(view) {
+            if (currentView) {
+                currentView.destroy();
+                currentView = null;
+            }
+    
+            switch (view) {
+            case 'Check':
+                background.setDialog('/dialog/dialog2');
+                currentView = check;
+                document.title = 'ファイルチェック';
+                break;
+            case 'Search':
+                background.setDialog('/dialog/dialog3');
+                currentView = search;
+                document.title = '検索';
+                break;
+            case 'Texture':
+                background.setDialog('/dialog/dialog4');
+                currentView = texturePreview;
+                document.title = 'テクスチャプレビュー';
+                break;
+            case 'Character':
+                background.setDialog('/dialog/dialog5');
+                currentView = characterEditor;
+                document.title = 'キャラクタエディタ';
+                break;
+            }
+    
+            if (currentView) {
+                currentView.show();
+            }
+        });
+    
+        function backPage(e) {
+            if (currentView === scenario) {
+                scenario.back();
+            }
+            else {
+                background.setDialog('/dialog/dialog1');
+                scenario.show();
+                currentView = scenario;
+                document.title = 'シナリオページ';
+            }
         }
+    
+        window.addEventListener('keydown', function(e) {
+            if (e.key == 'Escape') {
+                backPage();
+            }
+        });
+    
+        backPage();
+    });
+}
 
-        switch (view) {
-        case 'Check':
-            background.setDialog('/dialog/dialog2');
-            currentView = check;
-            document.title = 'ファイルチェック';
-            break;
-        case 'Search':
-            background.setDialog('/dialog/dialog3');
-            currentView = search;
-            document.title = '検索';
-            break;
-        case 'Texture':
-            background.setDialog('/dialog/dialog4');
-            currentView = texturePreview;
-            document.title = 'テクスチャプレビュー';
-            break;
-        case 'Character':
-            background.setDialog('/dialog/dialog5');
-            currentView = characterEditor;
-            document.title = 'キャラクタエディタ';
-            break;
-        }
+/**
+ * draw test page
+ * @param {typeof import('./sandica/adelite.js').default} Adelite 
+ */
+function drawTestPage(Adelite) {
+    const template = {
+        "link": {
+            "once:rel": "stylesheet",
+            "once:href": "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
+            "once:integrity": "sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk",
+            "once:crossorigin": "anonymous"
+        },
+        "div.container-fluid": {
+            "div.border row p-2": {
+                "div.col-2 inline": {
+                    "once:textContent": "BGRSPテストページ"
+                },
+                "div.col-3 inline": {
+                    "once:textContent": "v2.0.0"
+                }
+            }
+        },
+        "script#jquery": {
+            "once:src": "https://code.jquery.com/jquery-3.5.1.slim.min.js",
+            "once:integrity": "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj",
+            "once:crossorigin": "anonymous",
+            "on:load": "{{ jqueryReady() }}"
+        },
+        "script#bootstrap": {
+            "if": "{{ isJQueryReady }}",
+            "once:src": "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js",
+            "once:integrity": "sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI",
+            "once:crossorigin": "anonymous",
+        },    
+    };
 
-        if (currentView) {
-            currentView.show();
+    const adelite = new Adelite('#app', template);
+    adelite.show({
+        isJQueryReady: false,
+        jqueryReady() {
+            this.isJQueryReady = true;
+            adelite.update();
         }
     });
+    adelite.update();
 
-    function backPage(e) {
-        if (currentView === scenario) {
-            scenario.back();
-        }
-        else {
-            background.setDialog('/dialog/dialog1');
-            scenario.show();
-            currentView = scenario;
-            document.title = 'シナリオページ';
-        }
-    }
+    document.title = 'テストページ';
+}
 
-    window.addEventListener('keydown', function(e) {
-        if (e.key == 'Escape') {
-            backPage();
-        }
-    });
-
-    backPage();
-});
+if (BGRSP) {
+    Promise.all([
+        import('./components/background.js'),
+        import('./components/scenario.js'),
+        import('./components/check.js'),
+        import('./components/search.js'),
+        import('./components/texture-preview.js'),
+        import('./components/character-editor.js')
+    ]).then((modules) => drawPage(
+        modules[0].default,
+        modules[1].default,
+        modules[2].default,
+        modules[3].default,
+        modules[4].default,
+        modules[5].default));
+}
+else {
+    import('./sandica/adelite.js').then((module) => drawTestPage(module.default));
+}
